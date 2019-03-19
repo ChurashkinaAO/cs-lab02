540104a566c6bab52e064126e3504d0f144d0786
Alice(ChurashkinaAO) <alice@example.com>
2019-03-19 15:35:08 +0300

    code:изменили тип переменных на double

--- main.cpp
+++ main.cpp
@@ -5,14 +5,13 @@ using namespace std;
 int main()
 {
     cout<< "Enter A and B: ";
-    int a,b;
+    double a,b;
  cout << "A + B = " << a + b << '\n'
       << "A - B = " << a - b << '\n'
       << "A * B = " << a * b << '\n'
       << "A / B = " << a / b << '\n';
     if (a>b)
-        max=a;
-    cout<<'max= '<<max;
+    cout<<'max= '<<a;
 
     return 0;
 }
